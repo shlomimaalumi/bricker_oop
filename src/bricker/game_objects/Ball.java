84aeddb6d4058f19ece68d8a82d1194e88b2336b
shlomimaalumi <shlomom.maalumi@mail.huji.ac.il>
2024-01-31 14:36:44 +0200

    ball Changes by refael

--- src/bricker/game_objects/Ball.java
+++ src/bricker/game_objects/Ball.java
@@ -4,5 +4,36 @@ package bricker.game_objects;
 //        Vector2 newVel = getVelocity().flipped(collision.getNormal());
 //        setVelocity(newVel);
 
-public class Ball{
+import danogl.GameObject;
+import danogl.collisions.Collision;
+import danogl.gui.Sound;
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
+
+public class Ball extends GameObject {
+    private final Sound collisionSound;
+
+    /**
+     * Construct a new GameObject instance.
+     *
+     * @param topLeftCorner Position of the object, in window coordinates (pixels). Note that (0,0) is the
+     *                      top-left corner of the window.
+     * @param dimensions    Width and height in window coordinates.
+     * @param renderable    The renderable representing the object. Can be null, in which case the GameObject
+     *                      will not be rendered.
+     * @param sound
+     */
+
+    public Ball(Vector2 topLeftCorner, Vector2 dimensions, Renderable renderable, Sound sound) {
+        super(topLeftCorner, dimensions, renderable);
+        collisionSound = sound;
+    }
+
+    @Override
+    public void onCollisionEnter(GameObject other, Collision collision) {
+        super.onCollisionEnter(other, collision);
+        Vector2 newVel = getVelocity().flipped(collision.getNormal());
+        setVelocity(newVel);
+        collisionSound.play();
+    }
 }
